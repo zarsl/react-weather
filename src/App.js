ae2ec79b762c2f6ac7ec45596e8aa97cff1cba05
zarsl <64368356+zarsl@users.noreply.github.com>
2020-12-02 14:58:37 -0500

    Removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <p>
+        This project was coded by Zeynep Arslan and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/zarsl/react-weather"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          {" "}
+          open-sourced on Github
         </a>
-      </header>
+      </p>
     </div>
   );
 }
-
-export default App;
