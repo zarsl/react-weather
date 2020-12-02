9af5167308748b3d1739acaa29ee7424c3587b36
zarsl <64368356+zarsl@users.noreply.github.com>
2020-12-02 15:07:03 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        This project was coded by Zeynep Arslan and is{" "}
-        <a
-          href="https://github.com/zarsl/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <p>
+          This project was coded by Zeynep Arslan and is{" "}
+          <a
+            href="https://github.com/zarsl/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
