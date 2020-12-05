ec6afb8be61bc34d1171418321ea57f6ad1c919a
zarsl <64368356+zarsl@users.noreply.github.com>
2020-12-05 18:11:21 -0500

    Added mapping to Weather icon to use same icon set

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,9 +1,32 @@
 export default function WeatherIcon(props) {
-  let imgUrl = `http://openweathermap.org/img/wn/${props.icon}@2x.png`;
+  const codeMapping = {
+    "01d": "c01d",
+    "01n": "c01n",
+    "02d": "c02d",
+    "02n": "c02n",
+    "03d": "c03d",
+    "03n": "c03n",
+    "04d": "c04d",
+    "04n": "c04n",
+    "09d": "r04d",
+    "09n": "r04n",
+    "10d": "r04d",
+    "10n": "r04n",
+    "11d": "t04d",
+    "11n": "t04n",
+    "13d": "s01d",
+    "13n": "s01n",
+    "50d": "a05d",
+    "51n": "a05n",
+  };
+
+  let imgUrl = `https://www.weatherbit.io/static/img/icons/${
+    codeMapping[props.icon]
+  }.png`;
 
   return (
     <span className="WeatherIcon">
-      <img className="icon" src={imgUrl} alt={props.description} />
+      <img className="icon" src={imgUrl} alt={props.description}></img>
     </span>
   );
 }
