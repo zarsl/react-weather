8224cbd3126efbfebdaf59ab5f12582af17aa83f
zarsl <64368356+zarsl@users.noreply.github.com>
2020-12-05 22:37:11 -0500

    Added theme context for toggling between light and dark views

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import Weather from "./Weather";
 import WeatherUnitProvider from "./context/WeatherUnitContext";
 import TimeUnitProvider from "./context/TimeUnitContext";
 import "./styles/App.css";
+import { useTheme, useThemeUpdate } from "./context/ThemeContext";
+import ThemeProvider from "./context/ThemeContext";
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
 
 export default function App() {
+  const lightTheme = useTheme();
+  const toggleTheme = useThemeUpdate();
+
   return (
     <div className="App">
       <div className="container">
-        <WeatherUnitProvider>
-          <TimeUnitProvider>
-            <Weather defaultCity="New York" />
-            <footer>
-              This project was coded by Zeynep Arslan and is{" "}
-              <a
-                href="https://github.com/zarsl/react-weather"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {" "}
-                open-sourced on Github
-              </a>
-            </footer>
-          </TimeUnitProvider>
-        </WeatherUnitProvider>
+        <ThemeProvider>
+          <WeatherUnitProvider>
+            <TimeUnitProvider>
+              <Weather defaultCity="New York" />
+              <footer>
+                This project was coded by Zeynep Arslan and is{" "}
+                <a
+                  href="https://github.com/zarsl/react-weather"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  open-sourced on Github
+                </a>
+              </footer>
+              <Toggle
+                checked={lightTheme}
+                icons={false}
+                onChange={toggleTheme}
+              />
+            </TimeUnitProvider>
+          </WeatherUnitProvider>
+        </ThemeProvider>
       </div>
     </div>
   );
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,27 @@
+import React, { useState, useContext } from "react";
+
+const ThemeContext = React.createContext();
+const ThemeUpdateContext = React.createContext();
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
+
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext);
+}
+
+export default function ThemeProvider({ children }) {
+  const [lightTheme, setLightTheme] = useState(true);
+
+  function toggleTheme() {
+    setLightTheme((prevtheme) => !prevtheme);
+  }
+
+  return (
+      <ThemeUpdateContext.Provider value={toggleTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
+    </ThemeContext.Provider>
+  );
+}
